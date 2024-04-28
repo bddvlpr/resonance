3476b35a558d3abb2643de74aa903b1183cf6eeb
Luna Simons <luna@bddvlpr.com>
2024-04-28 13:12:07 +0200

    feat: system struct

--- systems/module.nix
+++ systems/module.nix
@@ -1,1 +1,23 @@
-{}
+{
+  self,
+  inputs,
+  lib,
+  withSystem,
+  ...
+}: let
+  inherit (self) outputs;
+  inherit (inputs.nixpkgs.lib) nixosSystem;
+
+  mkSystem = host: system:
+    withSystem system ({pkgs, ...}: {
+      "${host}" = nixosSystem {
+        inherit pkgs system;
+        specialArgs = {inherit inputs outputs;};
+        modules = [./${host} ./${host}/hardware.nix];
+      };
+    });
+in {
+  flake.nixosConfigurations = lib.mkMerge [
+    (mkSystem "apollo" "x86_64-linux")
+  ];
+}
