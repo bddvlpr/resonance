883523a8c6e33d27639dd1696cb846b11e74d378
Luna Simons <luna@bddvlpr.com>
2024-06-05 17:55:58 +0200

    fix(modules/home): firefox persistence

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -1,3 +1,12 @@
 {
   programs.firefox.enable = true;
+
+  home = {
+    sessionVariables.BROWSER = "firefox";
+    persistence."/persist/home/bddvlpr" = {
+      directories = [
+        ".mozilla/firefox"
+      ];
+    };
+  };
 }
