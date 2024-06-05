e36bb7cc0564806e2fe6c9cba67cc049eef5f1ec
Luna Simons <luna@bddvlpr.com>
2024-06-05 17:47:56 +0200

    fix(modules/home): git libsecret handling

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,6 +1,7 @@
-{
+{pkgs, ...}: {
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
 
     userName = "Luna Simons";
     userEmail = "luna@bddvlpr.com";
@@ -8,6 +9,10 @@
     signing = {
       key = "42EDAE8164B99C3A4B835711AB69B6F3380869A8";
       signByDefault = true;
+    };
+
+    extraConfig = {
+      credential.helper = "libsecret";
     };
   };
 }
