28fe533f02fed953bf242faf52804ae7a5f2eecc
Luna Simons <luna@bddvlpr.com>
2024-07-15 03:19:48 +0200

    feat(modules/home): manual swaylock :)

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -78,6 +78,7 @@ in {
           [
             "$mod, Q, killactive,"
             "$mod SHIFT, Q, exit,"
+            "$mod SHIFT, Insert, exec, ${getExe config.programs.swaylock.package}"
 
             "$mod, Return, exec, ${getExe pkgs.foot}"
             "$mod, SPACE, exec, ${getExe pkgs.wofi} -S drun"
