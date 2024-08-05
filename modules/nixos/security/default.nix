f6f85be4252c3368bfd58e4fbb995fb6b3f5deb9
Luna Simons <luna@bddvlpr.com>
2024-08-05 17:57:27 +0200

    fix(modules/nixos): fdlimit

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -1,5 +1,15 @@
 {
   security = {
-    pam.services.swaylock.fprintAuth = false;
+    pam = {
+      services.swaylock.fprintAuth = false;
+      loginLimits = [
+        {
+          domain = "*";
+          item = "nofile";
+          type = "-";
+          value = "524288";
+        }
+      ];
+    };
   };
 }
