ce6501055e8d2bc9fd653664344efe8191177aea
Luna Simons <luna@bddvlpr.com>
2026-01-09 12:46:19 +0100

    chore: librewolf persist cookies

--- users/bddvlpr/programs/librewolf/default.nix
+++ users/bddvlpr/programs/librewolf/default.nix
@@ -29,6 +29,24 @@
       PasswordManagerEnabled = false;
     };
 
+    settings = {
+      "privacy.clearOnShutdown.cache" = true;
+      "privacy.clearOnShutdown.cookies" = false;
+      "privacy.clearOnShutdown.downloads" = true;
+      "privacy.clearOnShutdown.formdata" = true;
+      "privacy.clearOnShutdown.history" = true;
+      "privacy.clearOnShutdown.offlineApps" = false;
+      "privacy.clearOnShutdown.openWindows" = true;
+      "privacy.clearOnShutdown.sessions" = true;
+      "privacy.clearOnShutdown.siteSettings" = false;
+      "privacy.clearOnShutdown_v2.browsingHistoryAndDownloads" = true;
+      "privacy.clearOnShutdown_v2.cache" = true;
+      "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+      "privacy.clearOnShutdown_v2.formdata" = true;
+      "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = true;
+      "privacy.clearOnShutdown_v2.siteSettings" = false;
+    };
+
     profiles = {
       personal = import ./personal args;
       work = import ./work args;
